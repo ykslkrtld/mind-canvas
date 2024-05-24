a1a2094e52ac670122c704406a6d751ae35032cd
yksl <yukselkurtuldu9@gmail.com>
2024-05-24 11:53:31 +0300

    edit

--- src/components/blog/Card.jsx
+++ src/components/blog/Card.jsx
@@ -12,15 +12,20 @@ import VisibilityIcon from '@mui/icons-material/Visibility';
 import { Button } from '@mui/material';
 import useBlogCalls from '../../services/useBlogCalls';
 import { useSelector } from "react-redux";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 const BlogCard = ({blog, currentPage}) => {
 
+  const navigate = useNavigate()
   const { postLikes } = useBlogCalls();
   const { users } = useSelector((state) => state.getBlog);
   const { user } = useSelector((state) => state.auth);
   console.log(user)
+
+  const handleLikes = () => {
+    user ? postLikes(blog._id, currentPage) : navigate("/login")
+  }
 
   return (
     <Card sx={{ width: 360, height: 450, display:"flex", flexDirection:"column", justifyContent:"space-between" }}>
@@ -41,7 +46,7 @@ const BlogCard = ({blog, currentPage}) => {
       </CardContent>
       <CardActions disableSpacing sx={{justifyContent:"space-between"}}>
         <CardActions>
-        <IconButton aria-label="add to favorites" onClick={() => postLikes(blog._id, currentPage)}>
+        <IconButton aria-label="add to favorites" onClick={handleLikes}>
         <FavoriteIcon sx={{ color: user && blog.likes.includes(users[0]?._id) ? "red" : "inherit" }} />
         <Typography>
           {blog?.likes.length}
